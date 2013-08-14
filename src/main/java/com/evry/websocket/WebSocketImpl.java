9a0ccfce73b5bdb9bca918c3fdc2145cda91d39b
Henrik Andersson <henrik.andersson3@evry.com>
2013-08-14 16:59:39 +0200

    start of implementation

---
+++ src/main/java/com/evry/websocket/WebSocketImpl.java
@@ -0,0 +1,10 @@
+package com.evry.websocket;
+
+import com.sun.grizzly.websockets.WebSocketEngine;
+
+public class WebSocketImpl {
+
+	public void execute() {
+		WebSocketEngine webSocketEngine = WebSocketEngine.getEngine();
+	}
+}
